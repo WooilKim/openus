dc40726d446beb278b5cafa74975c12dcd51a048
wooilkim <wooilkim@korea.ac.kr>
2019-01-14 22:34:14 +0900

    jquery html함수로 인해 selenium사용

--- crawler/congresspeople/currentmem.py
+++ crawler/congresspeople/currentmem.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
-import re
-
-
+from selenium import webdriver
+import time
 
 
 def current_mem():
@@ -14,65 +13,66 @@ def current_mem():
     # cur = conn.cursor()
     # cur.execute('create table ')
     # http: // www.assembly.go.kr / assm / memact / congressman / memCond / memCond.do
-    for page in range(1, 100):
-        params = {'strPage': page}
-        url = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCond.do"
+    driver = webdriver.Chrome('/Users/wooil/chromedriver')
+
+    page = 1
+    params = {"s_poly_cd": "",
+              "s_dept_cd": "",
+              "s_dtl_no": "",
+              "s_elected_method": "",
+              "s_up_orig_cd": "",
+              "s_dw_orig_cd": "",
+              "s_mem_nm": "", 'currentPage': page * 6 + 1, 'movePageNum:': '', 'rowPerPage': 6}
+
+    # params = {}
+    url = "http://www.assembly.go.kr/assm/memact/congressman/memCond/memCond.do"
+    driver.get(url)
+    driver.implicitly_wait(3)
+    for page in range(2, 51):
         print(page)
-        source = requests.post(url, data=params)
-        plain_text = source.text
-        # print(plain_text)
-        soup = bs(plain_text, 'lxml')
-        for title in soup.select('table > tbody > tr > td > a'):
-            if not title.has_attr('href'):
-                continue
-            # print(title)
-            # print(title.attrs['href'])
-            parent = title.parent.parent
-            print('의안번호', parent.contents[1].text)
-            print('제안구분', parent.contents[5].text)
-            print('제안일', parent.contents[7].text)
-            if not parent.contents[9].contents[1].has_attr('href'):
-                print('소관위', parent.contents[9].contents[1]['title'])
-            else:
-                print('소관위', parent.contents[9].contents[1]['title'], parent.contents[9].contents[1]['href'])
 
-            # for c in p.contents:
-            #     print(c)
-            # print('회부일', parent.findNext('td').findNext('td').findNext('td').text)
-            # print(parent.findNext('td').contents[1])
-            # print(parent.findNext('td').contents[2])
+        list = driver.find_element_by_class_name('memberna_list')
+        print(list.text)
+        paging = driver.find_element_by_class_name('pageing')
+        span = paging.find_element_by_tag_name('span').find_elements_by_tag_name('a')
+        if page % 10 == 1:
+            tmp = paging.find_elements_by_tag_name('a')
+            tmp[-2].click()
+            time.sleep(1)
+            page += 1
+            list = driver.find_element_by_class_name('memberna_list')
+            print(list.text)
+            paging = driver.find_element_by_class_name('pageing')
+            span = paging.find_element_by_tag_name('span').find_elements_by_tag_name('a')
 
-            l = list(re.findall("\'{1}[\w\d]*\'{1}", title.attrs['href'], re.S))
-            billID, opt = l[0][1:-1], l[1][1:-1]
-            print(title['title'], billID, opt)
-            # print(match.group(1))
+        # if page == 11:
+        #     print()
+        for a in span:
+            if int(a.text) == page:
+                a.click()
+                time.sleep(1)
+                break
+                # driver.implicitly_wait(100)
 
-            # print(title.attrs['href'].split('[\(\),]'))
-            # billID = title.attrs['href'][22:-25]
-            # print(billID)
-            detailurl = "http://likms.assembly.go.kr/bill/billDetail.do?billId=" + billID
-            detailsource = requests.get(detailurl)
-            detailsoup = bs(detailsource.text, 'lxml')
-            # for t in detailsoup.select('tbody > tr > td > a'):
-            #     if not t.has_attr('href'):
-            #         continue
-            # print(t['href'])
-            coactorurl = "http://likms.assembly.go.kr/bill/coactorListPopup.do?billId=" + billID
-            coactorsource = requests.get(coactorurl)
-            coactorsoup = bs(coactorsource.text, 'lxml')
-            for c in coactorsoup.select('a'):
-                # cname = re.split('\\(\\)', c.text)
-                name, remain = c.text.split('(')
-                party, remain = remain.split('/')
-                name_chinese, _ = remain.split(')')
-
-                print(name, party, name_chinese, c['href'][58:])
-            # print(coactorsource.text)
-            # http://likms.assembly.go.kr/bill/coactorListPopup.do?billId=PRC_V1Y9Y0B1F1N4V1I4M0L6V5Z0A3C4O7
+        #
+        # print(page)
+        # # source = requests.post(url, data=params)
+        #
+        # source = requests.get(url,params=params)
+        # plain_text = source.text
+        # # print(plain_text)
+        # soup = bs(plain_text, 'lxml')
+        # print(soup.find('div', {'id': 'listArea'}))
+        # for title in soup.find('div', {'class': 'memberna_list'}):
+        #     # if not title.has_attr('alt'):
+        #     #     continue
+        #
+        #     print(title)
+        #     # print(coactorsource.text)
+        # http://likms.assembly.go.kr/bill/coactorListPopup.do?billId=PRC_V1Y9Y0B1F1N4V1I4M0L6V5Z0A3C4O7
 
 
 # http://likms.assembly.go.kr/bill/billDetail.do?billId=PRC_V1Z9D0X1W1B0J1E7P5B9T1M6C4U5Q0
 # http://likms.assembly.go.kr/bill/coactorListPopup.do?billId=PRC_V1Z9D0X1W1B0J1E7P5B9T1M6C4U5Q0
 if __name__ == '__main__':
-    recent_bills()
-
+    current_mem()
