03688a70656ce6f3bab21c4e647ed829b5f6d840
wwwyyyy <97will@naver.com>
2021-01-14 03:11:33 +0900

    update detail

--- crawler/bills/detail.py
+++ crawler/bills/detail.py
@@ -1,52 +1,42 @@
 import requests
 from bs4 import BeautifulSoup as bs
-from bs4 import NavigableString
-from collections import defaultdict
 import re
 import pandas as pd
 import json
 
 
-url = "https://likms.assembly.go.kr/bill/billDetail.do?billId=ARC_A2V0B0A9F2I5O1O3X4N4A4O5W4X6O0"
+url = "https://likms.assembly.go.kr/bill/billDetail.do?billId=PRC_M2X0L0J9L1V4Z1W0L5P5A4L1X6A3M6"
 source = requests.post(url)
 soup = bs(source.text, 'lxml')
+# 의안 상세 페이지
 
-for caption in soup.find_all("caption"):
+for caption in soup.find_all("caption"): # 테이블 명 찾기
     #print(caption.text)
     if caption.text == '등록의견 리스트':
         continue
     parent = caption.parent
 
-    dic = {}
     th_list = []
     td_list = []
-    for th in parent.find_all('th'):
+    for th in parent.find_all('th'): # 테이블 column 찾기
         th_list.append(th.text)
     print(th_list)
-    for td in parent.find_all('td'):
+
+    for td in parent.find_all('td'): # 테이블 내용
         value = td.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('\xa0', '').replace(' ', '')
         td_list.append(value)
     print(td_list)
-    #d1 = zip(th_list, td_list)
-    # 복수의 td목록에 대해 하나만 추가되는 상황 ex) 관련위원회 - 정무위원회, 환경노동위원회, ... > 정무위원회
-    df = pd.DataFrame(columns = th_list)
+
+    df = pd.DataFrame(columns = th_list) # DataFrame화
     if len(td_list) > len(th_list):
-        print(len(td_list), len(th_list))
         for i in range(1, int(len(td_list)/len(th_list))):
             df.loc[i] = td_list[(i-1)*len(th_list):i*len(th_list)]
     else:
         df.loc[0] = td_list
-
-
-
-    #df = pd.(td_list)
     print(df)
-    #print(dict(d1))
+
     """
-    for i in range(len(parent.contents[5].contents[1])):
-        print(parent.contents[5].contents[1].contents[i])
-        print(parent.contents[7].contents[1].contents[i])
+    with open(f'./의안/{bill_num}.json', 'w') as f:
+        f.write(jsondict)
+        f.flush()
     """
-    #print(parent.contents[5].contents[1].contents[2].text) #thead
-    #print(parent.contents[7]) #tbody
-    #print(parent)
