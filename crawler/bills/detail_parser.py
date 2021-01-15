01e203be29919b7b59a75616ce968b6f63d4a985
wwwyyyy <97will@naver.com>
2021-01-16 03:41:53 +0900

    detail parser for 21st bills

---
+++ crawler/bills/detail_parser.py
@@ -0,0 +1,48 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import json
+
+#def load_file():
+f = open(f"21st_id.json", encoding="UTF-8")
+raw_data = json.loads(f.read())
+
+for i in range(0, 7016):
+    url = "https://likms.assembly.go.kr/bill/billDetail.do?billId=" + raw_data[i]["id"]
+    source = requests.post(url)
+    soup = bs(source.text, 'lxml')
+
+    table_dict = {}
+    bill_num = soup.select('table > tbody > tr > td')[0].text # 의안번호
+    #print(bill_num)
+    for caption in soup.find_all("caption"): # 테이블 명 찾기
+        #print(caption.text)
+        caption_text = caption.text
+        if caption.text == '등록의견 리스트':
+            continue
+        parent = caption.parent
+
+        th_list, td_list = [], []
+        for th in parent.find_all('th'): # 테이블 column 찾기
+            th_list.append(th.text)
+
+        for td in parent.find_all('td'): # 테이블 내용
+            value = td.text.replace('\t', '').replace('\n', '').replace('\r', '').replace('\xa0', '').replace(' ', '')
+            td_list.append(value)
+
+        df = pd.DataFrame(columns = th_list) # DataFrame화
+        if len(td_list) > len(th_list): # 복수의 테이블 내용에 대해 정리
+            for i in range(1, int(len(td_list)/len(th_list))):
+                df.loc[i] = td_list[(i-1)*len(th_list):i*len(th_list)]
+        else:
+            df.loc[0] = td_list
+
+        df_dict = df.to_dict('list')
+        #print(df_dict)
+        table_dict[caption_text] = df_dict
+
+    jsondict = json.dumps(table_dict, indent=4, ensure_ascii=False)
+    with open(f'./의안정리/{bill_num}.json', 'w') as f:
+        f.write(jsondict)
+        f.flush()
+
+    print(jsondict)
