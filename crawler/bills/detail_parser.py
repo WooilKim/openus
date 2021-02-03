5f61b7e4d03743941cb01ee82baeeec875ded4f3
wwwyyyy <97will@naver.com>
2021-02-04 03:24:17 +0900

    Update detail_parser.py

--- crawler/bills/detail_parser.py
+++ crawler/bills/detail_parser.py
@@ -4,6 +4,7 @@ import re
 import json
 import pandas as pd
 from datetime import date
+from requests import get
 import os
 
 age_list = ["01", "02", "03", "04", "05", "AA", "06", "07", "08", "BB", "09", "10", "CC", "11", "12", "13", "14", "15",
@@ -27,6 +28,10 @@ def detail_parser():
     for age in age_list:
         raw_data = load_file(age)
         id_count = len(raw_data)
+        # 저장 폴더 생성
+        folder = age + "_bill_v3"
+        if not os.path.exists(folder):
+            os.mkdir(folder)
 
         for i in range(id_count):
             url = "https://likms.assembly.go.kr/bill/billDetail.do?billId=" + raw_data[i]["id"]
@@ -75,7 +80,56 @@ def detail_parser():
                 ss = soup.select('div#summaryContentDiv')
                 table_dict['의안접수정보']['제안이유 및 주요내용'] = ss[0].text.strip()
 
+            # 의안 파일 다운로드
+            for a in soup.select('table > tbody > tr > td > a'):
+                #print(a)
+                href = a.attrs['href']
 
+                if 'openBillFile' in href: #파일 형식 hwp, pdf 2종류
+                    td = a.parent
+
+                    k = re.findall("\'{1}(.*?)\'{1}", href)
+                    #print(k)
+                    fileID = k[1]
+                    filetype = k[2] # 파일 형식 구분
+
+                    file_url = "http://likms.assembly.go.kr/filegate/servlet/FileGate?bookId=" + fileID + "&type=" + filetype
+                    print(file_url)
+
+                    #파일 폴더 생성
+                    file_folder = bill_num + "_file"
+                    folder_path = folder + "/" + file_folder
+                    if not os.path.exists(folder_path):
+                        os.mkdir(folder_path)
+
+                    if filetype == '0': # hwp 저장
+                        with open(f'./{folder}/{file_folder}/{fileID}.hwp', 'wb') as f:
+                            response = get(file_url)
+                            f.write(response.content)
+                            f.flush()
+                    elif filetype == '1': # pdf 저장
+                        with open(f'./{folder}/{file_folder}/{fileID}.pdf', 'wb') as f:
+                            response = get(file_url)
+                            f.write(response.content)
+                            f.flush()
+
+                elif 'ConfFile' in href: # pdf만 존재하는 형식
+                    k = re.findall("\'{1}(.*?)\'{1}", href)
+                    fileID = k[1]
+                    conf_url = "http://likms.assembly.go.kr/record/new/getFileDown.jsp?CONFER_NUM="+fileID
+                    print(conf_url)
+
+                    file_folder = bill_num + "_file"
+                    folder_path = folder + "/" + file_folder
+                    if not os.path.exists(folder_path):
+                        os.mkdir(folder_path)
+
+                    with open(f'./{folder}/{file_folder}/{fileID}.pdf', 'wb') as f:
+                        response = get(conf_url)
+                        f.write(response.content)
+                        f.flush()
+                else:
+                    continue
             #table_dict.update(date_dict)
 
             jsondict = json.dumps(table_dict, indent=4, ensure_ascii=False)
@@ -83,13 +137,10 @@ def detail_parser():
             # jsondict['parsed_date'] = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
             # print(jsondict)
 
-            # 저장 폴더 생성
-            folder = age + "_bill_v2"
-            if not os.path.exists(folder):
-                os.mkdir(folder)
+
 
             # 저장
-            with open(f'./{age}_bill_v2/{bill_num}_v2.json', 'w') as f:
+            with open(f'./{folder}/{bill_num}_v3.json', 'w') as f:
                 f.write(jsondict)
                 f.flush()
 
